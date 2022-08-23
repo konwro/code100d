e4c5ebdcdc4a4c9b571606a8e021f3f47739557c
konwro <73890368+konwro@users.noreply.github.com>
2022-08-23 22:06:14 +0200

    Create 12_h3ll0_th3r3.pl

---
+++ Perl/12_h3ll0_th3r3.pl
@@ -0,0 +1,6 @@
+#!usr/bin/perl
+print "Enter your name: ";
+$name=<STDIN>;
+print "Name what you value the most: ";
+$item=<STDIN>;
+print "HEll0, ${name} ... U w1ll s00n b3 91v1n9 up ${item} 4nd m0r3 45 4 n3w Perl 4ddict!%*51kk0";
